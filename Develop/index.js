d7cb8b63536f79c5b0fe776563a8f4486426000a
GTodd711 <150283557+GTodd711@users.noreply.github.com>
2024-02-18 10:50:17 -0800

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+// Table of Contents
+const questions = [
+    " What's the title of your project? ",
+    " How would you best describe your project? ",
+    " Is your application available for download? ",
+    " How would you guide someone through your project? ",
+    " Is the Project able to be tested? ",
+    " How do you want potential contributors to be able to ask questions about the application? ",
+    " What liscense's are you using for the project? ",
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
